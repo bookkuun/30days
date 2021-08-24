37621f56c54ed4214ebd2b42914114d30638e981
bookkuun <62459751+kuboyuya@users.noreply.github.com>
2021-08-24 19:50:04 +0900

    update: ログイン/ユーザー登録後/ログアウト後のリダイレクト先変更

--- 30days/routes/web.php
+++ 30days/routes/web.php
@@ -2,34 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+// user
 Route::get('/users/{id}', [App\Http\Controllers\UserController::class, 'show'])->name('show');
-
 Route::get('/profile', [App\Http\Controllers\UserController::class, 'edit'])->name('profile_edit');
 Route::post('/profile', [App\Http\Controllers\UserController::class, 'update'])->name('profile_update');
 Route::delete('/users/{id}', [App\Http\Controllers\UserController::class, 'destroy'])->name('user_delete');
 
+// challenge
 Route::post('/challenges', [App\Http\Controllers\ChallengeController::class, 'store'])->name('challenge_store');
 Route::get('/challenges/{challenge_id}', [App\Http\Controllers\ChallengeController::class, 'edit'])->name('challenge_edit');
 Route::post('/challenges/{challenge_id}', [App\Http\Controllers\ChallengeController::class, 'update'])->name('challenge_update');
 Route::delete('/challenges/{challenge_id}', [App\Http\Controllers\ChallengeController::class, 'destroy'])->name('challenge_delete');
 
-
+// diary
 Route::post('/diaries', [App\Http\Controllers\DiaryController::class, 'store'])->name('diary_store');
 Route::get('/diaries/{diary_id}', [App\Http\Controllers\DiaryController::class, 'edit'])->name('diary_edit');
 Route::post('/diaries/{diary_id}', [App\Http\Controllers\DiaryController::class, 'update'])->name('diary_update');
